508921cf065f50e06fc613b44e218a4873a12a86
Marcelo Hillesheim <48066124+MarceloHillesheim@users.noreply.github.com>
2019-08-23 13:10:58 -0300

    Update recebe.php

--- recebe.php
+++ recebe.php
@@ -1,9 +1,11 @@
 <?php
-#Backend
+/**
+ * Backend
+ * Aula 15/08/2019 - Recepção de dados no Back-end
+ */
+
+
 
 echo "<pre>\n";
-print_r($_GET);
-echo "</pre>\n";
-
-echo "\n<h1>Nome: $_GET[usuario]</h1>";
-echo "\n<h2>Senha: $_GET[senha]</h2>";
+print_r($_POST);
+echo "\n</pre>\n";
