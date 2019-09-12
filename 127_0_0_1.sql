1588f16c8a4b81407b10c24fd1a838ed39dbd950
Marcelo Hillesheim <marcelo_hillesheim1@estudante.sc.senai.br>
2019-09-12 16:39:18 -0300

    Preparação para gerar nova senha
    #12

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Set-2019 às 16:45
+-- Tempo de geração: 12-Set-2019 às 16:37
 -- Versão do servidor: 10.3.16-MariaDB
 -- versão do PHP: 7.3.7
 
@@ -38,17 +38,19 @@ CREATE TABLE `usuario` (
   `email` varchar(100) COLLATE utf8mb4_bin NOT NULL,
   `senha` char(40) COLLATE utf8mb4_bin NOT NULL,
   `dataCriacao` datetime NOT NULL,
-  `avatar_url` varchar(220) COLLATE utf8mb4_bin NOT NULL
+  `avatar_url` varchar(220) COLLATE utf8mb4_bin NOT NULL,
+  `token` char(10) COLLATE utf8mb4_bin NOT NULL,
+  `tempo_de_vida` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`idUsuario`, `nome`, `nomeUsuario`, `email`, `senha`, `dataCriacao`, `avatar_url`) VALUES
-(7, 'jonas', 'joninhas', 'jonas@jon', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 16:33:20', 'https://dok7xy59qfw9h.cloudfront.net/296/743/934/910003004-1s2o43a-8q0g09hrmi0mo0p/original/PassadodoSasuke.jpg'),
-(3, 'Robson', 'robson', 'rob@son', '435e1889d882d4d2737212f0dc8606818bc475e1', '2019-09-06 16:07:51', 'http://pm1.narvii.com/6425/744fc38b6108a6291056b437b68a248baa929c3f_00.jpg'),
-(6, 'marcelo', 'marceloo', 'marcelo@mar', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 16:27:36', 'https://i.pinimg.com/originals/7d/75/3a/7d753abc69dbf8e7b6e64487246e9f62.jpg');
+INSERT INTO `usuario` (`idUsuario`, `nome`, `nomeUsuario`, `email`, `senha`, `dataCriacao`, `avatar_url`, `token`, `tempo_de_vida`) VALUES
+(7, 'jonas', 'joninhas', 'jonas@jon', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 16:33:20', 'https://dok7xy59qfw9h.cloudfront.net/296/743/934/910003004-1s2o43a-8q0g09hrmi0mo0p/original/PassadodoSasuke.jpg', '', '2019-09-12 19:33:22'),
+(3, 'Robson', 'robson', 'rob@son', '435e1889d882d4d2737212f0dc8606818bc475e1', '2019-09-06 16:07:51', 'http://pm1.narvii.com/6425/744fc38b6108a6291056b437b68a248baa929c3f_00.jpg', '', '2019-09-12 19:33:22'),
+(6, 'marcelo', 'marceloo', 'marcelo@mar', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 16:27:36', 'https://i.pinimg.com/originals/7d/75/3a/7d753abc69dbf8e7b6e64487246e9f62.jpg', '', '2019-09-12 19:33:22');
 
 --
 -- Índices para tabelas despejadas
@@ -69,7 +71,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
